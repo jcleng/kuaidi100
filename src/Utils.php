77e719eddac1ca7e1b0d9da0b544253bf3619cc1
jcleng <j.c.leng@foxmail.com>
2018-07-05 10:07:31 +0800

    Update Utils.php

--- src/Utils.php
+++ src/Utils.php
@@ -30,15 +30,16 @@ class Utils
 
     private static function curlRequest($url)
     {
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_REFERER, $url);
-        curl_setopt($ch, CURLOPT_TIMEOUT, 5);
-        curl_setopt($ch, CURLOPT_BINARYTRANSFER, true);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:24.0) Gecko/20100101 Firefox/24.0');
-        $response = curl_exec($ch);
-        curl_close($ch);
+//         $ch = curl_init();
+//         curl_setopt($ch, CURLOPT_URL, $url);
+//         curl_setopt($ch, CURLOPT_REFERER, $url);
+//         curl_setopt($ch, CURLOPT_TIMEOUT, 5);
+//         curl_setopt($ch, CURLOPT_BINARYTRANSFER, true);
+//         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+//         curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:24.0) Gecko/20100101 Firefox/24.0');
+//         $response = curl_exec($ch);
+//         curl_close($ch);
+        $response = file_get_contents($url);
         return $response;
     }
 }
